0deeda6d5c3db944cd701d09e38e41a5fbb2031f
adiirani <148150663+adiirani@users.noreply.github.com>
2025-04-20 11:31:00 -0700

    Delete timeStepper directory

--- timeStepper/backend/levels/6/challenge.js
+++
@@ -1,8 +0,0 @@
-function reversePairs(input) {
-    let output = '';
-    for (let i = 0; i < input.length; i += 2) {
-        // Reverse the pair
-        output += input.charAt(i + 1) + input.charAt(i);
-    }
-    return output;
-}
--- timeStepper/backend/levels/7/challenge.js
+++
@@ -1,9 +0,0 @@
-const xorKey = 0x04; // Simple small key
-
-function transform(inputString) {
-    let output = "";
-    for (let i = 0; i < input.length; i++) {
-        output += String.fromCharCode(input.charCodeAt(i) ^ xorKey);
-    }
-    return output;
-}
